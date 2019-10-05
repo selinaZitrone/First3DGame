e13bf31eb6ef1f0e8f18d7f526ebb63a317c8c52
Selina Baldauf <selina.baldauf@gmx.de>
2019-10-05 18:21:52 +0200

    simple countdown as coroutine

---
+++ Assets/CountDown.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using TMPro;
+
+public class CountDown : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timeLabel = null;
+    [SerializeField] int countdownTime = 60;
+    private void Start()
+    {
+        StartCoroutine("CountDownTime");
+        Time.timeScale = 1;
+    }
+
+    private void Update()
+    {
+        if(countdownTime <= 10)
+        {
+            timeLabel.color = Color.red;
+        }
+
+        timeLabel.text = ("" + countdownTime);
+
+        if (countdownTime == 0)
+        {
+            StopCoroutine("CountDownTime");
+            EventManager.TriggerEvent("death");
+        }       
+    }
+
+    IEnumerator CountDownTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            countdownTime--;
+        }
+    }
+
+}
